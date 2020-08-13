d2acaf2d34c4a2cfb42b19441fc9f5efb50046b6
Trevis McGill <trevismcgill25@gmail.com>
2020-08-13 20:12:05 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -20,9 +20,15 @@ class Song
 
   def self.new_by_name(arg)
     song = self.new
+<<<<<<< HEAD
     # binding.pry
     song.name = arg
     song
+=======
+    song.name = arg
+    # binding.pry
+    # song
+>>>>>>> a056bc34012dc0695706cdc2c96c212f110f7458
   end
 
   def self.create_by_name(arg)
@@ -34,6 +40,7 @@ class Song
   end
 
   def self.find_by_name(arg)
+<<<<<<< HEAD
     self.all.find{|song| song.name == arg}
   end
 
@@ -67,6 +74,25 @@ class Song
       song.name = filename[1].delete_suffix(".mp3")
       song.save
       song
+=======
+    self.all.find{|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(arg)
+
+  end
+
+  def self.alphabetical(arg)
+
+  end
+
+  def self.new_from_filename(arg)
+
+  end
+
+  def self.create_from_filename(arg)
+
+>>>>>>> a056bc34012dc0695706cdc2c96c212f110f7458
   end
 
   def self.destroy_all
